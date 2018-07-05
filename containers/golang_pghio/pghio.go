884458d2afae2ffcc7769d8f2a82e6c0bbf3d9d1
Peter Gallerani <peter.gallerani@gmail.com>
2018-07-05 12:52:27 -0600

    add something of a prompt to the landing page, close #9

--- containers/golang_pghio/pghio.go
+++ containers/golang_pghio/pghio.go
@@ -3,13 +3,14 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
 )
 
 var pghioHitCount = 0
 
 func main() {
-	fmt.Println("Starting pg-h.io")
+	log.Println("Starting pg-h.io")
 	http.HandleFunc("/favicon.ico", favicon)
 	http.HandleFunc("/", pghio)
 	http.HandleFunc("/metrics", metrics)
@@ -17,13 +18,44 @@ func main() {
 }
 
 func pghio(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.New("index.html")
-	tmpl, err := tmpl.ParseFiles("html/index.html")
-	if err != nil {
-		fmt.Println("ERROR: pghio tmpl.ParseFiles", err)
+	if r.Method == "GET" {
+		tmpl := template.New("index.html")
+		tmpl, err := tmpl.ParseFiles("html/index.html")
+		if err != nil {
+			log.Println("ERROR: pghio tmpl.ParseFiles", err)
+		}
+		tmpl.Execute(w, "")
+		pghioHitCount++
+		log.Println(r.RemoteAddr, "requested", r.RequestURI)
 	}
-	tmpl.Execute(w, "")
-	pghioHitCount++
+	if r.Method == "POST" {
+		r.ParseForm() // need to parse the form before interacting with form data
+		text := r.Form["text"]
+		switch text[0] {
+		case "pg --blog":
+			http.Redirect(w, r, "http://blog.pg-h.io/", http.StatusFound)
+		case "pg --blog-metrics":
+			http.Redirect(w, r, "http://blog.pg-h.io/metrics", http.StatusFound)
+		case "pg --github":
+			http.Redirect(w, r, "https://github.com/silentpete", http.StatusFound)
+		case "pg --grafana":
+			http.Redirect(w, r, "http://grafana.pg-h.io/", http.StatusFound)
+		case "pg --grafana-metrics":
+			http.Redirect(w, r, "http://grafana.pg-h.io/metrics", http.StatusFound)
+		case "pg --help":
+			http.Redirect(w, r, "/", http.StatusFound)
+		case "pg -h":
+			http.Redirect(w, r, "/", http.StatusFound)
+		case "pg --prometheus":
+			http.Redirect(w, r, "http://prometheus.pg-h.io/", http.StatusFound)
+		case "pg --prometheus-metrics":
+			http.Redirect(w, r, "http://prometheus.pg-h.io/metrics", http.StatusFound)
+		case "pg --resume":
+			http.Redirect(w, r, "https://www.linkedin.com/in/petegallerani/", http.StatusFound)
+		default:
+			http.Redirect(w, r, "/", http.StatusFound)
+		}
+	}
 }
 
 func metrics(w http.ResponseWriter, r *http.Request) {

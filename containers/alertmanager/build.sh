1a7e0e4d4c6ba0e2c0526d6b1ac0b246d7194a7a
Peter Gallerani <peter.gallerani@gmail.com>
2018-07-10 17:27:23 -0600

    add alertmanager

---
+++ containers/alertmanager/build.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+CWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+file="${CWD}/gmail_creds"
+alertmanagerYml="${CWD}/alertmanager.yml"
+
+# check for file
+if [[ -f "${file}" ]]; then
+  # source file
+  . ${file}
+else
+  # create file with gmail info used for connecting alertmanager to gmail for alerts
+  touch ${file}
+  # Set gmail email and app password for alertmanager
+  echo "What is your gmail email address alertmanager should send emails to?"
+  read GMAIL_ACCOUNT
+  echo -e "export GMAIL_ACCOUNT=${GMAIL_ACCOUNT}" >> ${file}
+  echo "What is the gmail app password to use?"
+  read GMAIL_APP_PASSWORD
+  echo -e "export GMAIL_APP_PASSWORD=${GMAIL_APP_PASSWORD}" >> ${file}
+  echo "What is the gmail smtp server to use [smtp.gmail.com:587]"
+  read GMAIL_SMTP_SERVER
+  echo -e "export GMAIL_SMTP_SERVER=${GMAIL_SMTP_SERVER}" >> ${file}
+
+  # source file
+  . ${file}
+fi
+
+if [[ ! -z "${GMAIL_ACCOUNT}" ]]; then
+  sed -i "s|REPLACE_W_GMAIL_ACCOUNT|${GMAIL_ACCOUNT}|" ${alertmanagerYml}
+fi
+if [[ ! -z "${GMAIL_APP_PASSWORD}" ]]; then
+  sed -i "s|REPLACE_W_GMAIL_APP_PASSWORD|${GMAIL_APP_PASSWORD}|" ${alertmanagerYml}
+fi
+if [[ ! -z "${GMAIL_SMTP_SERVER}" ]]; then
+  sed -i "s|REPLACE_W_GMAIL_SMTP_SERVER|${GMAIL_SMTP_SERVER}|" ${alertmanagerYml}
+fi

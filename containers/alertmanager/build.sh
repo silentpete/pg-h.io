92551ea65ce4d150b904cde36195b84fd1c9edb1
Peter Gallerani <peter.gallerani@gmail.com>
2018-07-12 18:16:08 -0600

    update docker containers to latest versions
    - also add some error checking in the alertmanager build script

--- containers/alertmanager/build.sh
+++ containers/alertmanager/build.sh
@@ -26,12 +26,32 @@ else
   . ${file}
 fi
 
+success="true"
 if [[ ! -z "${GMAIL_ACCOUNT}" ]]; then
   sed -i "s|REPLACE_W_GMAIL_ACCOUNT|${GMAIL_ACCOUNT}|" ${alertmanagerYml}
+fi
+err="$?"
+if [[ ! $err == 0 ]]; then
+  echo -e "error code: $err, replacing gmail account"
+  success="false"
 fi
 if [[ ! -z "${GMAIL_APP_PASSWORD}" ]]; then
   sed -i "s|REPLACE_W_GMAIL_APP_PASSWORD|${GMAIL_APP_PASSWORD}|" ${alertmanagerYml}
 fi
+err="$?"
+if [[ ! $err == 0 ]]; then
+  echo -e "error code: $err, replacing gmail app password"
+  success="false"
+fi
 if [[ ! -z "${GMAIL_SMTP_SERVER}" ]]; then
   sed -i "s|REPLACE_W_GMAIL_SMTP_SERVER|${GMAIL_SMTP_SERVER}|" ${alertmanagerYml}
+fi
+err="$?"
+if [[ ! $err == 0 ]]; then
+  echo -e "error code: $err, replacing gmail smtp server"
+  success="false"
+fi
+
+if [[ $success != "true" ]]; then
+  exit 1
 fi

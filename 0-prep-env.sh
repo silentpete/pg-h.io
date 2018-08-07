1773d4e954347b05f0f2f328c12bb512a037bf7d
Peter Gallerani <peter.gallerani@gmail.com>
2018-08-06 21:53:52 -0600

    test ability to quickly deploy to a new host

--- 0-prep-env.sh
+++ 0-prep-env.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-# TODO: remove repeated function into function file and source the file
 function info() {
   echo -e "\e[32mINFO: $1\e[0m"
 }
@@ -14,13 +13,61 @@ function error() {
   exit 1
 }
 
+function install_docker_ce() {
+  info "install docker-ce"
+  sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+  sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+  sudo yum-config-manager --enable docker-ce-edge
+  sudo yum install -y docker-ce
+  sudo systemctl enable docker
+  sudo systemctl start docker
+}
+
+function install_docker_compose() {
+  info "install docker-compose"
+  sudo curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+  sudo curl -L https://raw.githubusercontent.com/docker/compose/1.16.1/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
+}
+
+# create_container_storage takes two parameters, the container name and port.
+# A /data/<container_name> directory will be created.
+# A group and user will be created
+# arg 1: container name
+# arg 2: container port
+function create_container_storage() {
+  if [[ ! ${#} -eq 2 ]]; then
+    error "firewalld_add_service: received invalid number of arguments: ${#}"
+  fi
+
+  container=${1,,}
+  uid=${2}
+  gid=${uid}
+  c_data_path="${data_storage_path}/${container}"
+
+  if [[ ! -d "${c_data_path}" ]]; then
+    info "creating ${c_data_path}"
+    sudo mkdir "${c_data_path}"
+  fi
+
+  if [[ ! $uid -eq 0 ]]; then
+    info "create ${container} group"
+    sudo groupadd ${container} -g ${gid}
+    # sudo useradd -u ${uid} -g ${gid} ${gid}
+  fi
+
+  info "set permissions on directory"
+  sudo chown ${uid}:${gid} -R "${c_data_path}"
+
+}
+
 # firewalld_add_service is used to create a services xml file which will be added.
 # After creating the file, the firewalld service config will be reloaded.
 # arg 1: service name
 # arg 2: port number
 function firewalld_add_service() {
   if [[ ! ${#} -eq 2 ]]; then
-    error "firewalld_add_service: received invalid number of arguments: ${}#"
+    error "firewalld_add_service: received invalid number of arguments: ${#}"
   fi
 
   if [[ ! $(firewall-cmd --state) -eq "running" ]]; then
@@ -31,32 +78,34 @@ function firewalld_add_service() {
   port="$2"
   f_path="/etc/firewalld/services"
 
-  if [[ -f "${f_path}/${service}.xml"  ]]; then
+  if [[ -f "${f_path}/${service}.xml" ]]; then
     warn "${service}.xml already exists, guessing it is already allowed"
   else
+    info "creating firewalld service file ${service}.xml"
+    contents="<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<service>\n  <short>${service}</short>\n  <description>${service}</description>\n  <port protocol=\"tcp\" port=\"${port}\"/>\n</service>"
+    touch "${f_path}/${service}.xml"
+    echo -e "${contents}" > "${f_path}/${service}.xml"
+    # firewalld takes 5 seconds to see the newly added file
+    # reference: https://firewalld.org/documentation/howto/add-a-service.html
+    sleep 5
 
-  info "creating firewalld service file ${service}.xml"
-  contents="<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<service>\n  <short>${service}</short>\n  <description>${service}</description>\n  <port protocol=\"tcp\" port=\"${port}\"/>\n</service>"
-  touch "${f_path}/${service}.xml"
-  echo -e "${contents}" > "${f_path}/${service}.xml"
-  # firewalld takes 5 seconds to see the newly added file
-  # reference: https://firewalld.org/documentation/howto/add-a-service.html
-  sleep 5
+    info "firewalld adding service ${service}"
+    firewall-cmd --permanent --add-service=${service}
+    if [[ $? -ne 0 ]]; then
+      error "firewalld add service failed"
+    fi
 
-  info "firewalld adding service ${service}"
-  firewall-cmd --permanent --add-service=${service}
-  if [[ $? -ne 0 ]]; then
-    error "firewalld add service failed"
-  fi
-
-  info "firewalld reload services"
-  firewall-cmd --reload
-  if [[ $? -ne 0 ]]; then
-    error "firewalld reload services failed"
-  fi
-
+    info "firewalld reload services"
+    firewall-cmd --reload
+    if [[ $? -ne 0 ]]; then
+      error "firewalld reload services failed"
+    fi
   fi
 }
+
+# Setup the Docker environment.
+install_docker_ce
+install_docker_compose
 
 # Run all build.sh files
 for VAR in $(find . -type f -name "build.sh"); do
@@ -64,56 +113,17 @@ for VAR in $(find . -type f -name "build.sh"); do
   ${VAR}
 done
 
+# Keep state of containers on host (I know, that's it's own debate)
 data_storage_path="/data"
-
-# Keep state of container on host (I know, that's it's own debate)
 if [[ ! -d "${data_storage_path}" ]]; then
-  info 'creating /data'
+  info "creating ${data_storage_path}"
   sudo mkdir "${data_storage_path}"
 fi
 
-# Each container could be wrapped in a sourced function
-# prometheus data
-container="prometheus"
-c_data_path="${data_storage_path}/${container}"
-uid=9090
-gid=9090
-if [[ ! -d "${c_data_path}" ]]; then
-  info "creating ${c_data_path}"
-  sudo mkdir "${c_data_path}"
-fi
-info "set ${container} permissions"
-sudo groupadd -r ${gid}
-sudo useradd -u ${uid} -g ${gid} ${gid}
-sudo chown ${uid}:${gid} -R "${c_data_path}"
-
-# grafana data
-container="grafana"
-c_data_path="${data_storage_path}/${container}"
-uid=472 # 5.1.0 change to running as 472:472
-gid=472
-if [[ ! -d "${c_data_path}" ]]; then
-  info "creating ${c_data_path}"
-  sudo mkdir "${c_data_path}"
-fi
-info "set ${container} permissions"
-sudo groupadd -r ${gid}
-sudo useradd -u ${uid} -g ${gid} ${gid}
-sudo chown ${uid}:${gid} -R "${c_data_path}"
-
-# influxdb data
-container="influxdb"
-c_data_path="${data_storage_path}/${container}"
-uid=0
-gid=0
-if [[ ! -d "${c_data_path}" ]]; then
-  info "creating ${c_data_path}"
-  sudo mkdir "${c_data_path}"
-fi
-info "set ${container} permissions"
-# sudo groupadd -r ${gid}
-# sudo useradd -u ${uid} -g ${gid} ${gid}
-sudo chown ${uid}:${gid} -R "${c_data_path}"
+# Create the host data storage location
+create_container_storage "prometheus" "9090"
+create_container_storage "grafana" "472"
+create_container_storage "influxdb" "0"
 
 # Add allowed services to firewalld
 firewalld_add_service "cadvisor" "8181"
--- README.md
+++ README.md
@@ -7,3 +7,13 @@ I work within a linux vagrant box all day long for the most part. I ask programs
 ## what is the reason for this site?
 
 I have always wanted to help others and I love what I do for work everyday. I would like to basically try to give out anything I've learned in the broad categories of IT, and this will be where I start writing those things out. I also like to continue learning, so this will also be a place where I can attempt to learn more.
+
+## Linode Setup
+
+I am currently running this site on Linode.com. I am used to working with CentOS 7, so that is the OS for the host I'm writing this for. The host is the minimum template with 1 CPU, 1GB of memory, and 20/25GB of Disk.
+
+1. Stand up Linode Host and SSH in
+1. Install GIT
+1. Git clone ([https://github.com/silentpete/pg-h.io.git](https://github.com/silentpete/pg-h.io.git))
+1. Run the Prep Script
+1. Run the Start Script

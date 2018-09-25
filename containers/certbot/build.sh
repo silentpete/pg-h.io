8a0ce5770ba63788f16d028c39e0022f6269d9cc
Peter Gallerani <peter.gallerani@gmail.com>
2018-09-25 12:29:01 -0600

    implement SSL with letsencrypt

---
+++ containers/certbot/build.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+CWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if [[ $(ls -1 ../nginx/certs/ | wc -l) -eq 0 ]]; then
+  if [[ -z "${GMAIL_ACCOUNT}" ]]; then
+    echo "What is your email address that should be used for letsencrypt?"
+    read GMAIL_ACCOUNT
+  else
+    # TODO: make this state the name and confirm to use it or set alternate
+    info "found and using email address: ${GMAIL_ACCOUNT}"
+  fi
+  sudo docker run -it --rm --name certbot -v $PWD/etc/letsencrypt:/etc/letsencrypt:rw -v $PWD/var/lib/letsencrypt:/var/lib/letsencrypt:rw certbot/certbot:v0.27.1 certonly --server https://acme-v02.api.letsencrypt.org/directory --expand --manual --preferred-challenges dns -d pg-h.io -d *.pg-h.io --agree-tos -m ${GMAIL_ACCOUNT}
+  rc=$?
+  if [[ ${rc} -ne 0 ]]; then
+    error "certbot failed"
+  fi
+  cp etc/letsencrypt/live/pg-h.io/* ../nginx/certs/
+fi
